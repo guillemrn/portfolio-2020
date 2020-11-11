ffffb0236ecdfaa1dd6946a870eb842d974c3801
Guillermo Moreno <contact@memobrown.com>
2020-11-11 16:33:59 -0600

    Tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+export const theme = {
+  extend: {
+    colors: {
+      primary: {
+        lighter: '#FFA5AD',
+        default: '#FF4B5C',
+        dark: '#FF5D6C',
+      },
+      secondary: {
+        lighter: '#82B3BA',
+        default: '#056674',
+        dark: '#1E7582',
+      },
+      third: {
+        lighter: '#B3DFDF',
+        default: '#66BFBF',
+        dark: '#75C5C5',
+      },
+      darkTheme: '#1F2E35',
+      lightTheme: '#F2FCFF',
+      textDark: '#1A3B34',
+      textLight: '#F9FEFF',
+    },
+  },
+};
