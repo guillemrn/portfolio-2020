04788495a313a9f6ac90d1449e6f3478052b4d58
Guillermo Moreno <contact@memobrown.com>
2020-11-12 14:58:14 -0600

    Hero.vue and Navigation.Vue added

---
+++ components/Hero.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex justify-between">
+    <div class="flex items-center flex-col">
+      <h1 class="text-6xl text-left text-textDark">
+        Soy Guillermo Moreno<br />diseñador UI y <br />Desarrollador web
+      </h1>
+      <button
+        class="bg-primary hover:bg-primary-lighter text-white font-bold text-4xl py-4 px-4 rounded-2xl"
+      >
+        Conoce mi trabajo
+      </button>
+    </div>
+    <div>
+      <div class="designBox absolute z-0 inset-y-0 right-0"></div>
+      <figure class="absolute z-10 bottom-0 img-boy">
+        <img src="https://i.imgur.com/2SMFTff.png" alt="Boy on a computer" />
+      </figure>
+      <!-- <figure>
+        <img src="https://i.imgur.com/Qjt8s2v.png" alt="" />
+      </figure>
+      <figure>
+        <img src="https://i.imgur.com/LMNeZPB.png" alt="" />
+      </figure>
+      <figure>
+        <img src="https://i.imgur.com/GYl9AzW.png" alt="" />
+      </figure> -->
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.designBox {
+  width: 40%;
+  height: 100vh;
+  background-color: theme('colors.darkTheme');
+}
+h1 {
+  font-family: 'Fredoka One', cursive;
+}
+.img-boy {
+  right: 10%;
+}
+</style>
---
+++ components/Navigation.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex justify-between">
+    <figure class="p-4 m-4">
+      <a href="https://memobrown.com" target="_blank"
+        ><img src="https://i.imgur.com/dwTDVmu.png" alt="Logo MemoBrown"
+      /></a>
+    </figure>
+    <figure class="p-4 m-4 relative z-10">
+      <img src="https://i.imgur.com/vLLi05z.png" alt="Menu" />
+    </figure>
+  </div>
+</template>
