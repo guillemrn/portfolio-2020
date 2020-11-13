8f7b3f509f6d91e78f5c895af2936db0ac653af2
Guillermo Moreno <contact@memobrown.com>
2020-11-12 20:59:03 -0600

    Hero.vue component finished

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,28 +1,39 @@
 <template>
-  <div>
-    <div>
+  <div class="container">
+    <div class="hero-title">
       <h1>Soy Guillermo Moreno<br />diseñador UI y <br />Desarrollador web</h1>
       <button>Conoce mi trabajo</button>
     </div>
     <div>
       <div class="designBox"></div>
-      <figure>
+      <figure class="boy">
         <img src="https://i.imgur.com/VZZoC1O.png" alt="Boy on a computer" />
       </figure>
-      <!-- <figure>
+      <figure class="diamond">
         <img src="https://i.imgur.com/Qjt8s2v.png" alt="" />
       </figure>
-      <figure>
+      <figure class="red-circle">
         <img src="https://i.imgur.com/LMNeZPB.png" alt="" />
       </figure>
-      <figure>
+      <figure class="dark-circle">
         <img src="https://i.imgur.com/GYl9AzW.png" alt="" />
-      </figure> -->
+      </figure>
     </div>
   </div>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+}
+.boy img {
+  position: relative;
+  z-index: 1;
+  bottom: 15rem;
+  transform: scale(1.1);
+}
 .designBox {
   width: 40%;
   height: 100vh;
@@ -31,7 +42,49 @@
   top: 0;
   right: 0;
 }
+.hero-title {
+  display: flex;
+  flex-flow: column;
+  align-self: start;
+  margin-top: 15rem;
+}
 h1 {
   font-family: var(--hero-buttons);
+  color: var(--textDark);
+  font-size: 5.5rem;
+  font-weight: 400;
+}
+button {
+  background-color: var(--primary-color);
+  color: #fff;
+  font-family: var(--hero-buttons);
+  padding: 2rem 6rem;
+  font-size: 2.5rem;
+  border-radius: 20px;
+  border: none;
+  outline-color: var(--primary-lighter);
+  transition: all 300ms;
+  width: fit-content;
+}
+button:hover {
+  background-color: var(--secondary-color);
+  cursor: pointer;
+}
+.diamond,
+.red-circle,
+.dark-circle {
+  position: absolute;
+}
+.diamond {
+  top: 10%;
+  left: 20%;
+}
+.red-circle {
+  right: 0;
+  top: 40%;
+}
+.dark-circle {
+  left: 0;
+  top: 80%;
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -15,6 +15,6 @@ export default {
 </script>
 <style>
 body {
-  background-color: theme('colors.lightTheme');
+  background-color: var(--lightTheme);
 }
 </style>
